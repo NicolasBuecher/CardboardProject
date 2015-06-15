41a3a760a102b25ce060415f041228cf8a298383
NicolasBuecher <buecher.nicolas@gmail.com>
2015-06-15 16:25:23 +0200

    fullscreen achievement

--- js/test.js
+++ js/test.js
@@ -1,93 +1,89 @@
 /**
- * Created by Nicolas Buecher on 12/06/2015
+ * Created by buecher on 15/06/15.
  */
 
-/* Demander à l'utilisateur l'autorisation de passer en mode plein écran */
-document.getElementById('fullscreen-button').addEventListener('click', function () {
-    if (screenfull.enabled) {
-        screenfull.request(document.getElementById('vr-area'));
-        document.body.removeChild('message-container');
+/* Gestion de l'événement "click sur le bouton OK" */
+
+document.getElementById('fullscreen-button').addEventListener('click', function ()
+{
+    if (screenfull.enabled)             // Si le navigateur autorise le mode plein écran
+    {
+        document.body.removeChild(document.getElementById('message-container'));        // Suppression des balises restantes
+        screenfull.request(document.body);                                              // Activation du mode plein écran
+        init();                                                                         // Initialisation du rendu
+        animate();                                                                      // Lancement de la boucle de rendu
     }
-    else {
-        document.getElementById('message-container').removeChild('fullscreen-button');
-        document.getElementById('message').innerHTML = 'Désolé, votre navigateur enmpêche le passage en mode plein écran.';
+    else                                // Si le navigateur bloque le mode plein écran
+    {
+        document.getElementById('message').innerHTML = 'Désolé, votre navigateur bloque le mode plein écran.';      // Affichage d'un message d'explications
+        document.getElementById('message-container').removeChild(document.getElementById('fullscreen-button'));     // Suppression du bouton OK
     }
 });
 
 /* Définition des variables globales */
 
-var width = window.innerWidth;              // Largeur de la fenêtre
-var height = window.innerHeight;            // Hauteur de la fenêtre
+var width, height;
+var camera, scene, renderer;
+var stereoEffect, orbitControls;
+var canvasElement, canvasContainer;
+var hemisphereLight, pointLight;
 
 var clock = new THREE.Clock();              // Initialisation d'un timer
 
-var camera, scene, renderer;
-var stereoEffect, orbitControls;
-var element, container;
-var hemisphereLight, pointLight;
-
-/* Déclaration des fonctions */
-
-init();
-animate();
+/* Fonction d'initialisation des données */
 
 function init()
 {
+
     /* Initialisation du renderer */
-
     renderer = new THREE.WebGLRenderer({ alpha: 1, antialias: true, clearColor: 0xffffff });    // WebGL Renderer, objets opaques, anticrénelage activé, couleur par défaut blanc
-    renderer.setSize(width, height);                                                            // Taille du renderer égale à la taille de la fenêtre
-    document.body.appendChild(renderer.domElement);                                                         // Binding, la zone de rendu se situe dans le body de index.html
 
-    /* Initialisation du container */
+    /* Initialisation du canvas */
 
-    element = renderer.domElement;                                      // Initialisation de element
-    container = document.body;                                          // Initialisation du container
-    container.appendChild(element);                                     // Binding???
+    canvasElement = renderer.domElement;                    // Récupération de l'élément HTML de la zone de rendu
+    canvasContainer = document.body;                        // Récupération de l'élément body
+    canvasContainer.appendChild(canvasElement);             // Application de la zone de rendu comme nouveau fils de l'élément body
 
     /* Initialisation de la scène */
     scene = new THREE.Scene();
 
-    /* Initialisation des effets */
-
+    /* Initialisation de l'effet stéréo */
     stereoEffect = new THREE.StereoEffect(renderer);
-    stereoEffect.separation = 0.2;
-    stereoEffect.targetDistance = 50;
-    stereoEffect.setSize(width, height);
 
     /* Initialisation de la caméra */
 
-    camera = new THREE.PerspectiveCamera(45, width/height, 0.1, 20000);        // Caméra en vue perspective, FOV de 45 degrés, ratio w/h, intervalle de profondeur [0.1, 20000]
+    camera = new THREE.PerspectiveCamera(45, canvasContainer.offsetWidth / canvasContainer.offsetHeight, 0.1, 20000);                   // Caméra en vue perspective, FOV de 45 degrés, ratio w/h, intervalle de profondeur [0.1, 20000]
     camera.position.set(0, 100, 150);                                          // Position de la caméra +0X +100Y +150Z
     scene.add(camera);                                                         // Ajout de la caméra à la scène
 
     /* Initialisation des contrôles */
-    orbitControls = new THREE.OrbitControls(camera, element);
 
-    /*
-    function setOrientationControls(e) {
-        if (!e.alpha) {
+    orbitControls = new THREE.OrbitControls(camera, canvasElement);         // Contrôles à la souris
+
+    function setOrientationControls(e)                                      // Contrôles pour l'orientation du mobile
+    {
+
+        if (!e.alpha)
+        {
             return;
         }
 
-        controls = new THREE.DeviceOrientationControls(camera, true);
-        controls.connect();
-        controls.update();
+        orbitControls = new THREE.DeviceOrientationControls(camera, true);
+        orbitControls.connect();
+        orbitControls.update();
 
-        element.addEventListener('click', fullscreen, false);
+        canvasElement.addEventListener('click', fullscreen, false);
 
         window.removeEventListener('deviceorientation', setOrientationControls);
+
     }
-    window.addEventListener('deviceorientation', setOrientationControls, true);*/
+
+    window.addEventListener('deviceorientation', setOrientationControls, true);     // Ajout d'un événement "Orientation du mobile"
 
     /* Initialisation de la lumière */
 
-    hemisphereLight = new THREE.HemisphereLight(0x777777, 0x000000, 0.6);       // Skycolor : gray, Groundcolor : black, Intensity : 0.6
-    scene.add(hemisphereLight);                                                           // Ajout de la lumière à la scène
-
-    pointLight = new THREE.PointLight(0x00ff00);        // Lumière ponctuelle verte
-    pointLight.position.set(0,800,-50);                 // Position de la lumière +0X +800Y -50Z
-    scene.add(pointLight);                              // Ajout de la lumière à la scène
+    hemisphereLight = new THREE.HemisphereLight(0x777777, 0x000000, 0.6);           // Skycolor : gray, Groundcolor : black, Intensity : 0.6
+    scene.add(hemisphereLight);                                                     // Ajout de la lumière à la scène
 
     /* Initialisation de la texture principale */
 
@@ -127,28 +123,28 @@ function init()
     var cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);              // Création du mesh
     cubeMesh.rotation.y = Math.PI * 45 / 180;                               // Rotation de PI/4 autour de l'axe Y
     cubeMesh.position.set(0, 25, 0);                                        // Position affectée à +0X +25Y +0Z
-    scene.add(cubeMesh);                                                    // Ajout dy lesh à la scène
+    scene.add(cubeMesh);                                                    // Ajout du mesh à la scène
 
-    /*window.addEventListener('resize', resize, false);
-    setTimeout(resize, 1);*/
+    window.addEventListener('resize', resize, false);               // Ajout d'un événement "redimensionner"
+    setTimeout(resize, 1);                                          // Initialisation de la taille de la fenêtre de rendu
 
 }
-/*
-function resize() {
-    var width = container.offsetWidth;
-    var height = container.offsetHeight;
+
+function resize()
+{
+
+    width = canvasContainer.offsetWidth;
+    height = canvasContainer.offsetHeight;
 
     camera.aspect = width / height;
     camera.updateProjectionMatrix();
 
     renderer.setSize(width, height);
-    effect.setSize(width, height);
-}*/
+    stereoEffect.setSize(width, height);
+}
 
 function update(dt)
 {
-
-    //resize();
 
     camera.updateProjectionMatrix();
 
@@ -156,26 +152,20 @@ function update(dt)
 
 }
 
-function render(dt) {
+function render(dt)
+{
+
     stereoEffect.render(scene, camera);
+
 }
 
-function animate(t) {
+function animate(t)
+{
+
     requestAnimationFrame(animate);
 
     update(clock.getDelta());
-    render(clock.getDelta());
-}
 
-function fullscreen()
-{
-    if (container.requestFullscreen) {
-        container.requestFullscreen();
-    } else if (container.msRequestFullscreen) {
-        container.msRequestFullscreen();
-    } else if (container.mozRequestFullScreen) {
-        container.mozRequestFullScreen();
-    } else if (container.webkitRequestFullscreen) {
-        container.webkitRequestFullscreen();
-    }
+    render(clock.getDelta());
+
 }
